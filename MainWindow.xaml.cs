ab13a0479d25e1e163709bdf2db77d272d2777b6
ray-adas2 <1533735779@qq.com>
2026-07-21 13:48:05 +0800

    添加系统托盘图标

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,26 +1,65 @@
-﻿using IndustrialMonitor.ViewModels;
-using System.Text;
+using IndustrialMonitor.ViewModels;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace IndustrialMonitor;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
+    private readonly MainViewModel _viewModel;
+    private readonly System.Windows.Forms.NotifyIcon _trayIcon;
+    private bool _isReallyClosing;
+
     public MainWindow(MainViewModel viewModel)
     {
         InitializeComponent();
-
+        _viewModel = viewModel;
         DataContext = viewModel;
+
+        // 系统托盘图标
+        _trayIcon = new System.Windows.Forms.NotifyIcon
+        {
+            Icon = global::System.Drawing.Icon.ExtractAssociatedIcon(
+                System.Reflection.Assembly.GetExecutingAssembly().Location)!,
+            Text = "工业监控平台",
+            Visible = true
+        };
+
+        // 右键菜单
+        var menu = new System.Windows.Forms.ContextMenuStrip();
+        menu.Items.Add("显示主窗口", null, (s, e) => { Show(); WindowState = WindowState.Normal; Activate(); });
+        menu.Items.Add("退出登录", null, (s, e) => { Show(); _viewModel.LogoutCommand.Execute(null); });
+        menu.Items.Add("-");
+        menu.Items.Add("关闭系统", null, (s, e) =>
+        {
+            _isReallyClosing = true;
+            _trayIcon.Visible = false;
+            _trayIcon.Dispose();
+            Application.Current.Shutdown();
+        });
+        _trayIcon.ContextMenuStrip = menu;
+
+        // 双击托盘图标
+        _trayIcon.DoubleClick += (s, e) => { Show(); WindowState = WindowState.Normal; Activate(); };
+
+        // 点 X 最小化到托盘
+        Closing += (s, e) =>
+        {
+            if (!_isReallyClosing)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+        };
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+        if (!_isReallyClosing)
+        {
+            _trayIcon.Visible = false;
+            _trayIcon.Dispose();
+        }
+        base.OnClosed(e);
     }
 }
